65e976be9e79919064a716421a4498cc4bd9a83a
Venura Shyamal <shamalbandara14@gmail.com>
2023-03-24 14:35:59 +0530

    added camera

--- WebotsArena_V2/2.Arena/controllers/my_keyboad_controller/build/release/my_keyboad_controller.d
+++ WebotsArena_V2/2.Arena/controllers/my_keyboad_controller/build/release/my_keyboad_controller.d
@@ -6,4 +6,6 @@ build/release/my_keyboad_controller.o: my_keyboad_controller.cpp \
  C:\Program\ Files\Webots/include/controller/cpp/webots/Robot.hpp \
  C:\Program\ Files\Webots/include/controller/cpp/webots/Keyboard.hpp \
  C:\Program\ Files\Webots/include/controller/cpp/webots/GPS.hpp \
- C:\Program\ Files\Webots/include/controller/cpp/webots/InertialUnit.hpp
+ C:\Program\ Files\Webots/include/controller/cpp/webots/InertialUnit.hpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Camera.hpp \
+ C:/Program\ Files/Webots/include/controller/c/webots/camera_recognition_object.h
--- WebotsArena_V2/2.Arena/controllers/my_keyboad_controller/my_keyboad_controller.cpp
+++ WebotsArena_V2/2.Arena/controllers/my_keyboad_controller/my_keyboad_controller.cpp
@@ -4,6 +4,7 @@
 #include <webots/Keyboard.hpp>
 #include <webots/GPS.hpp>
 #include <webots/InertialUnit.hpp>
+#include <webots/Camera.hpp>
 
 #define TIME_STEP 64
 using namespace webots;
@@ -11,14 +12,19 @@ using namespace webots;
 int main(int argc, char **argv) {
   Robot *robot = new Robot();
   Keyboard kb;
-  DistanceSensor *ds[5];
+  DistanceSensor *ds[6];
   
-  char dsNames[5][20] = {"ds front right", "ds front left","ds side right", "ds side left", "ds upper"};
-  for (int i = 0; i < 5; i++) {
+  char dsNames[6][20] = {"ds side right1","ds side right2", "ds front left","ds side left1", "ds side left2","ds front right"};
+  for (int i = 0; i < 6; i++) {
     ds[i] = robot->getDistanceSensor(dsNames[i]);
     ds[i]->enable(TIME_STEP);
   }
   
+  Camera *cam;
+cam= robot->getCamera("camera");
+cam->enable(TIME_STEP);
+cam->recognitionEnable(TIME_STEP);
+
   
   //updown motor
   Motor *lr;
@@ -36,13 +42,13 @@ int main(int argc, char **argv) {
     Motor *ba;
   ba=robot->getMotor("back_arm");
   
-  // GPS *gp;
-  // gp=robot->getGPS("global");
-  // gp-> enable(TIME_STEP);
+   GPS *gp;
+   gp=robot->getGPS("gps");
+   gp-> enable(TIME_STEP);
   
-  // InertialUnit *iu;
-  // iu=robot->getInertialUnit("imu");
-  // iu-> enable(TIME_STEP);
+  InertialUnit *iu;
+   iu=robot->getInertialUnit("inertial unit");
+   iu-> enable(TIME_STEP);
 
 
   
@@ -87,13 +93,14 @@ int main(int argc, char **argv) {
     wheels[1]->setVelocity(rightSpeed);
     wheels[2]->setVelocity(leftSpeed);
     wheels[3]->setVelocity(rightSpeed);
-    // std::cout<<"X : "<<gp->getValues()[0]<<std::endl;
-    // std::cout<<"Y : "<<gp->getValues()[1]<<std::endl;
-    // std::cout<<"Z : "<<gp->getValues()[2]<<std::endl;
-    // std::cout<<"########################"<<std::endl;
-    // std::cout<<"Angle X : "<<iu->getRollPitchYaw()[0]<<std::endl;
-    // std::cout<<"Angle Y : "<<iu->getRollPitchYaw()[1]<<std::endl;
-    // std::cout<<"Angle Z : "<<iu->getRollPitchYaw()[2]<<std::endl;
+    std::cout<<"X : "<<gp->getValues()[0]<<std::endl;
+    std::cout<<"Y : "<<gp->getValues()[1]<<std::endl;
+     std::cout<<"Z : "<<gp->getValues()[2]<<std::endl;
+     std::cout<<"########################"<<std::endl;
+    std::cout<<"Angle X : "<<iu->getRollPitchYaw()[0]<<std::endl;
+     std::cout<<"Angle Y : "<<iu->getRollPitchYaw()[1]<<std::endl;
+     std::cout<<"Angle Z : "<<iu->getRollPitchYaw()[2]<<std::endl;
+     std::cout<<"########################"<<std::endl;
     if (key==87 && linear<0.19){
     linear += 0.005;
     } else if (key==83 && linear>0){
